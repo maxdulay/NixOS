18dd1390af6c8f01ea92cdd1dbf9ea71ea195975
Max D <maxdulay@gmail.com>
2024-05-14 11:10:16 -0400

    Added wlogout to homemanager

--- configuration.nix
+++ configuration.nix
@@ -367,6 +367,56 @@
 
       };
     };
+    programs.wlogout = {
+      enable = true;
+      style = ''
+        * {
+        	background-image: none;
+        }
+        window {
+        	background-color: rgba(12, 12, 12, 0.0);
+        }
+        button {
+            color: #6F87E0;
+        	background-color: rgba(0, 0, 0, 0.8);
+        	/* border-style: solid;
+        	border-width: 2px; */
+        	background-repeat: no-repeat;
+        	background-position: center;
+        	background-size: 25%;
+        }
+
+        button:focus, button:active, button:hover {
+        	color: #000000;
+        	background-color: #6F87E0;
+        	outline-style: none;
+        }
+
+        #lock {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/lock.png"));
+        }
+
+        #logout {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/logout.png"));
+        }
+
+        #suspend {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/suspend.png"));
+        }
+
+        #hibernate {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/hibernate.png"));
+        }
+
+        #shutdown {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/shutdown.png"));
+        }
+
+        #reboot {
+            background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/reboot.png"));
+        }
+        	    '';
+    };
     programs.waybar = {
       enable = true;
       settings = {
